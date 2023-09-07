1e471d0b87f44c302e83cc868ed5fe42a3f424a7
Replit user <>
2023-09-07 17:35:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,9 +45,10 @@ void insertMap(HashMap * map, char * key, void * value) {
   }
   int posicion = hash(key,map->capacity);
 
-  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key == NULL){
+  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL){
     posicion = (posicion +1) % map->capacity;
   }
+
   
 }
 
